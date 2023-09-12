65650e65a8178147c46d74a411bdbd039c3ad6d3
DamaKostolo <teodaniil2@gmail.com>
2023-09-12 15:22:30 +0300

    подготовка к деплою

--- Dz_bot/create_bot.py
+++ Dz_bot/create_bot.py
@@ -1,9 +1,13 @@
+import config
+
 from aiogram import Bot
+
+
 from aiogram.dispatcher import Dispatcher
 import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token= config.TOKEN)
 dp = Dispatcher(bot, storage=storage)
